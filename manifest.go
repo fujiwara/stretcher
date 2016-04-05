dd84cf22b22ab377622503177469519e0c99bdc0
Uchio KONDO <udzura@pepabo.com>
2016-04-05 11:55:52 +0900

    Use just os.Remove()

--- manifest.go
+++ manifest.go
@@ -115,28 +115,9 @@ func (m *Manifest) Deploy(conf Config) error {
 
 	from := dir + "/"
 	to := m.Dest
-	// append "/" when not terminated by "/"
-	if strings.LastIndex(to, "/") != len(to)-1 {
-		to = to + "/"
-	}
+	log.Printf("Rename tmpdir %s to %s\n", from, to)
+	err = os.Rename(from, to)
 
-	args := []string{}
-	args = append(args, RsyncDefaultOpts...)
-	if m.ExcludeFrom != "" {
-		args = append(args, "--exclude-from", from+m.ExcludeFrom)
-	}
-	if len(m.Excludes) > 0 {
-		for _, ex := range m.Excludes {
-			args = append(args, "--exclude", ex)
-		}
-	}
-	args = append(args, from, to)
-
-	log.Println("rsync", args)
-	out, err = exec.Command("rsync", args...).CombinedOutput()
-	if len(out) > 0 {
-		log.Println(string(out))
-	}
 	if err != nil {
 		return err
 	}
